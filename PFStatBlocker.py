cd202704218e0f3cd7b1b07ec44985244c9f7e02
Teigan Penna <Teigan Penna>
2020-10-19 22:06:08 +1100

    use model name as filename

--- PFStatBlocker.py
+++ PFStatBlocker.py
@@ -1,16 +1,27 @@
 from jinja2 import Environment, FileSystemLoader
 
-def get_template():
-  with open('template.txt') as f:
-    return Template(f.read())
+def require_answer(question):
+  answer = input('* ' + question)
+  if answer:
+    return answer
+  raise 'This field is required!'
+
+def get_model():
+  model = {}
+  model['name'] = require_answer('Name: ')
+  return model
 
 def main():
+  model = get_model()
+
   env = Environment(loader=FileSystemLoader('templates'))
   template = env.get_template('statblock.txt')
+  
   stat_block = template.render()
-  with open('kanjougas.htm', 'w') as f:
+  filename = model['name'] + '.htm'
+  with open(filename, 'w') as f:
     f.write(stat_block)
-  print('Stat block kanjougas.htm created')
+  print('Stat block ' + filename + ' created')
 
 if __name__ == '__main__':
   main()
