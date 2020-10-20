762f1e303714ef821e0ed3d20c5bd9944c18710b
TeiganPenna <teigan.penna@gmail.com>
2020-10-20 18:08:59 +1100

    clean up inputs

--- PFStatBlocker.py
+++ PFStatBlocker.py
@@ -1,35 +1,39 @@
 from jinja2 import Environment, FileSystemLoader
 
-def require_answer(question):
-  answer = input('* ' + question)
-  if answer:
-    return answer
-  raise 'This field is required!'
+def required_field(field, model, question = ''):
+  value = ''
+  if question:
+    value = input('* ' + question + ': ')
+  else:
+    value = input('* ' + field.title() + ': ')
+
+  if value:
+    model[field] = value
+  else:
+    raise Exception('This field is required!')
+
+def optional_field(field, model, question = ''):
+  value = ''
+  if question:
+    value = input(question + ': ')
+  else:
+    value = input(field.title() + ': ')
+
+  if value:
+    model[field] = value
 
 def get_model():
   print('Fields with a * are required.')
   model = {}
-  model['name'] = require_answer('Name: ')
 
-  cr = input('CR: ')
-  if cr:
-    model['cr'] = 'CR: ' + cr
-
-  gender = input('Gender: ')
-  if gender:
-    model['gender'] = gender
-
-  race = input('Race: ')
-  if race:
-    model['race'] = race
-
-  classes = input('Class(es): ')
-  if classes:
-    model['class'] = classes
-
-  model['alignment'] = require_answer('Alignment: ')
-  model['size'] = require_answer('Size: ')
-  model['type'] = require_answer('Type: ')
+  required_field('name', model)
+  optional_field('cr', model, 'CR')
+  optional_field('gender', model)
+  optional_field('race', model)
+  optional_field('class', model, 'Class(es)')
+  required_field('alignment', model)
+  required_field('size', model)
+  required_field('type', model)
 
   return model
 
